7d0c5dde7c09490e6709c0bff668c700c93c7728
eliakip <kiplagateliakim52@gmail.com>
2025-06-01 21:49:56 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -21,7 +21,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  BOT_NAME: process.env.BOT_NAME || "JINX-MD",
+  BOT_NAME: process.env.BOT_NAME || "ELIAKIM-XMD",
   MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/6852sf.jpg",
   DESCRIPTION: process.env.DESCRIPTION || "ELIAKIM-XMD",
   OWNER_NAME: process.env.OWNER_NAME || "ELIAKIM",
